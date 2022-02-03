d4c6efcedf2df72901cc4b4007c7f98814187958
harshkc <harsh.hkc1006@gmail.com>
2022-02-04 00:08:22 +0530

    integrates CRUD question API

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar";
 import ToastNotification from "./components/ToastNotification";
+import Routes from "./pages/Routes";
 
 const App = () => {
   return (
     <div>
       <ToastNotification />
       <NavBar />
+      <Routes />
     </div>
   );
 };
