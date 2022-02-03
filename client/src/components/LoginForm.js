55bbd372f958bed8cc055eb7f7cb2c4ab2aea2a8
harshkc <harsh.hkc1006@gmail.com>
2022-02-03 23:52:07 +0530

    fixes navbar

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,55 +1,49 @@
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../graphql/mutations';
-import { useAuthContext } from '../context/auth';
-import { useStateContext } from '../context/state';
-import ErrorMessage from './ErrorMessage';
-import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { getErrorMsg } from '../utils/helperFuncs';
+import {useState} from "react";
+import {useForm} from "react-hook-form";
+import {useMutation} from "@apollo/client";
+import {LOGIN_USER} from "../graphql/mutations";
+import {useAuthContext} from "../context/auth";
+import {useStateContext} from "../context/state";
+import ErrorMessage from "./ErrorMessage";
+import * as yup from "yup";
+import {yupResolver} from "@hookform/resolvers/yup";
+import MpeaceLogo from "../svg/mpeace-logo.png";
+import {getErrorMsg} from "../utils/helperFuncs";
 
-import {
-  TextField,
-  Button,
-  Typography,
-  InputAdornment,
-  IconButton,
-  Link,
-} from '@material-ui/core';
-import { useAuthFormStyles } from '../styles/muiStyles';
-import PersonIcon from '@material-ui/icons/Person';
-import LockIcon from '@material-ui/icons/Lock';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
-import VisibilityIcon from '@material-ui/icons/Visibility';
+import {TextField, Button, Typography, InputAdornment, IconButton, Link} from "@material-ui/core";
+import {useAuthFormStyles} from "../styles/muiStyles";
+import PersonIcon from "@material-ui/icons/Person";
+import LockIcon from "@material-ui/icons/Lock";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
+import VisibilityIcon from "@material-ui/icons/Visibility";
 
 const validationSchema = yup.object({
-  username: yup.string().required('Required'),
-  password: yup.string().required('Required'),
+  username: yup.string().required("Required"),
+  password: yup.string().required("Required"),
 });
 
-const LoginForm = ({ setAuthType, closeModal }) => {
+const LoginForm = ({setAuthType, closeModal}) => {
   const [showPass, setShowPass] = useState(false);
   const [errorMsg, setErrorMsg] = useState(null);
   const classes = useAuthFormStyles();
-  const { setUser } = useAuthContext();
-  const { notify } = useStateContext();
-  const { register, handleSubmit, reset, errors } = useForm({
-    mode: 'onTouched',
+  const {setUser} = useAuthContext();
+  const {notify} = useStateContext();
+  const {register, handleSubmit, reset, errors} = useForm({
+    mode: "onTouched",
     resolver: yupResolver(validationSchema),
   });
 
-  const [loginUser, { loading }] = useMutation(LOGIN_USER, {
+  const [loginUser, {loading}] = useMutation(LOGIN_USER, {
     onError: (err) => {
       setErrorMsg(getErrorMsg(err));
     },
   });
 
-  const onLogin = ({ username, password }) => {
+  const onLogin = ({username, password}) => {
     loginUser({
-      variables: { username, password },
-      update: (_, { data }) => {
+      variables: {username, password},
+      update: (_, {data}) => {
         setUser(data.login);
         notify(`Welcome, ${data.login.username}! You're logged in.`);
         reset();
@@ -60,22 +54,26 @@ const LoginForm = ({ setAuthType, closeModal }) => {
 
   return (
     <div className={classes.root}>
+      <img src={MpeaceLogo} alt='mpeace-logo' className={classes.titleLogo} />
+      <Typography variant='h5' className={classes.title}>
+        <strong>M</strong>Peace
+      </Typography>
       <form onSubmit={handleSubmit(onLogin)}>
         <div className={classes.inputField}>
           <TextField
             fullWidth
             inputRef={register}
-            name="username"
-            type="text"
-            label="Username"
-            variant="outlined"
-            size="small"
-            error={'username' in errors}
-            helperText={'username' in errors ? errors.username.message : ''}
+            name='username'
+            type='text'
+            label='Username'
+            variant='outlined'
+            size='small'
+            error={"username" in errors}
+            helperText={"username" in errors ? errors.username.message : ""}
             InputProps={{
               startAdornment: (
-                <InputAdornment position="start">
-                  <PersonIcon color="primary" />
+                <InputAdornment position='start'>
+                  <PersonIcon color='primary' />
                 </InputAdornment>
               ),
             }}
@@ -86,58 +84,52 @@ const LoginForm = ({ setAuthType, closeModal }) => {
             required
             fullWidth
             inputRef={register}
-            name="password"
-            type={showPass ? 'text' : 'password'}
-            label="Password"
-            variant="outlined"
-            size="small"
-            error={'password' in errors}
-            helperText={'password' in errors ? errors.password.message : ''}
+            name='password'
+            type={showPass ? "text" : "password"}
+            label='Password'
+            variant='outlined'
+            size='small'
+            error={"password" in errors}
+            helperText={"password" in errors ? errors.password.message : ""}
             InputProps={{
               endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    onClick={() => setShowPass((prevState) => !prevState)}
-                    size="small"
-                  >
+                <InputAdornment position='end'>
+                  <IconButton onClick={() => setShowPass((prevState) => !prevState)} size='small'>
                     {showPass ? (
-                      <VisibilityOffIcon color="secondary" />
+                      <VisibilityOffIcon color='secondary' />
                     ) : (
-                      <VisibilityIcon color="secondary" />
+                      <VisibilityIcon color='secondary' />
                     )}
                   </IconButton>
                 </InputAdornment>
               ),
               startAdornment: (
-                <InputAdornment position="start">
-                  <LockIcon color="primary" />
+                <InputAdornment position='start'>
+                  <LockIcon color='primary' />
                 </InputAdornment>
               ),
             }}
           />
         </div>
         <Button
-          color="primary"
-          variant="contained"
-          size="large"
+          color='primary'
+          variant='contained'
+          size='large'
           fullWidth
           startIcon={<ExitToAppIcon />}
-          type="submit"
+          type='submit'
           disabled={loading}
         >
           Log In
         </Button>
       </form>
-      <Typography variant="body1" className={classes.footerText}>
-        Don’t have an account?{' '}
-        <Link onClick={() => setAuthType('signup')} className={classes.link}>
+      <Typography variant='body1' className={classes.footerText}>
+        Don’t have an account?{" "}
+        <Link onClick={() => setAuthType("signup")} className={classes.link}>
           Sign Up
         </Link>
       </Typography>
-      <ErrorMessage
-        errorMsg={errorMsg}
-        clearErrorMsg={() => setErrorMsg(null)}
-      />
+      <ErrorMessage errorMsg={errorMsg} clearErrorMsg={() => setErrorMsg(null)} />
     </div>
   );
 };
---
+++ client/src/components/NavMenuMobile.js
@@ -0,0 +1,77 @@
+import {useState} from "react";
+import {useLocation, Link as RouterLink} from "react-router-dom";
+
+import {IconButton, Menu, MenuItem} from "@material-ui/core";
+import {useMenuStyles} from "../styles/muiStyles";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
+import PublicIcon from "@material-ui/icons/Public";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import PeopleIcon from "@material-ui/icons/People";
+
+const MobileNavMenu = () => {
+  const {pathname} = useLocation();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const classes = useMenuStyles();
+
+  const handleOpenMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <IconButton color='primary' onClick={handleOpenMenu}>
+        {!anchorEl ? <MenuIcon /> : <CloseIcon className={classes.closeIcon} />}
+      </IconButton>
+
+      <Menu
+        anchorEl={anchorEl}
+        getContentAnchorEl={null}
+        anchorOrigin={{vertical: "bottom", horizontal: "left"}}
+        transformOrigin={{vertical: "top", horizontal: "left"}}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleCloseMenu}
+        marginThreshold={0}
+        elevation={1}
+      >
+        <MenuItem
+          selected={pathname === "/" || (!pathname.startsWith("/tag") && !pathname.startsWith("/user"))}
+          dense
+          component={RouterLink}
+          to='/'
+          onClick={handleCloseMenu}
+        >
+          <PublicIcon className={classes.menuIcon} />
+          Home
+        </MenuItem>
+        <MenuItem
+          selected={pathname.startsWith("/tag")}
+          dense
+          component={RouterLink}
+          to='/tags'
+          onClick={handleCloseMenu}
+        >
+          <LocalOfferIcon className={classes.menuIcon} />
+          Tags
+        </MenuItem>
+        <MenuItem
+          selected={pathname.startsWith("/user")}
+          dense
+          component={RouterLink}
+          to='/users'
+          onClick={handleCloseMenu}
+        >
+          <PeopleIcon className={classes.menuIcon} />
+          Users
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+};
+
+export default MobileNavMenu;
