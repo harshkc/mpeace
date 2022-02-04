496c57bb9118af5b1cb1479584641638adc29b90
harshkc <harsh.hkc1006@gmail.com>
2022-02-04 16:17:02 +0530

    adds qraphql API fetching methods

--- client/src/graphql/fragments.js
+++ client/src/graphql/fragments.js
@@ -1,19 +1,53 @@
 import {gql} from "@apollo/client";
 
+export const AUTHOR_DETAILS = gql`
+  fragment AuthorDetails on Author {
+    id
+    username
+  }
+`;
+
+export const ANSWER_DETAILS = gql`
+  fragment AnswerDetails on Answer {
+    id
+    author {
+      ...AuthorDetails
+    }
+    body
+    comments {
+      ...CommentDetails
+    }
+    points
+    upvotedBy
+    downvotedBy
+    createdAt
+    updatedAt
+  }
+  ${AUTHOR_DETAILS}
+`;
+
 export const QUESTION_DETAILS = gql`
   fragment QuestionDetails on Question {
     id
+    author {
+      ...AuthorDetails
+    }
     title
     body
     tags
     points
     views
     acceptedAnswer
+    answers {
+      ...AnswerDetails
+    }
     upvotedBy
     downvotedBy
     createdAt
     updatedAt
   }
+  ${AUTHOR_DETAILS}
+  ${ANSWER_DETAILS}
 `;
 
 export const LOGGED_USER_DETAILS = gql`
