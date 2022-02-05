800f550f8bbe09a08bfb8e70aa440ad4e69696f6
Harsh Choudhary <harsh.hkc1006@gmail.com>
2022-02-05 16:51:01 +0530

    chore:preparing for deployment (#14)

--- server/db.js
+++ server/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
-// const { MONGODB_URI: url } = require('./utils/config');
+const {MONGODB_URI: url} = require("./utils/config");
 
 const connectToDB = async () => {
+  //MongoDB local url : mongodb://127.0.0.1:27017/mpeace
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/mpeace", {
+    await mongoose.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
